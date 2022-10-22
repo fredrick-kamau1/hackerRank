03bb9a27c042e4643e625c30034afef874fb350e
fred <fredricoinc@gmail.com>
2022-10-22 15:57:40 -0500

    edits

--- src/hackerRank/twoDArrays.java
+++ src/hackerRank/twoDArrays.java
@@ -4,15 +4,39 @@ import java.util.Arrays;
 
 public class twoDArrays {
     public static void main(String[] args) {
-        int[][] nums = {{1,2,3},{4,5,6}};
-        System.out.println(Arrays.deepToString(nums));
+        int[][] nums = {{1,2,3},{4,5,6},{7,8,9}};
+        //System.out.println(Arrays.deepToString(nums));
+        arrPrint(nums);
+        System.out.println(arrSum(nums));
 
+    }
+
+    public static void arrPrint(int[][] nums){
         for(int row = 0; row < nums.length; row++){
             for (int col = 0; col < nums[row].length ; col++){
                 System.out.print(nums[row][col]);
             }
             System.out.println();
-            System.out.println();
         }
+    }
+    public static int arrSum(int[][] arr){
+        int rows = arr.length;
+        int columns = arr[0].length;
+
+        int max_hourglass_sum = Integer.MIN_VALUE;
+
+        for(int i = 0; i < rows-2; i++){
+            for (int j = 0; j < columns-2 ; j++){
+                int current_hourglass_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] +
+                                                        arr[i+1][j+1] +
+                                            arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                max_hourglass_sum = Math.max(max_hourglass_sum, current_hourglass_sum);
+                /*if(current_hourglass_sum> max_hourglass_sum){
+                    max_hourglass_sum = current_hourglass_sum;
+                }*/
+            }
+        }
+
+        return max_hourglass_sum;
     }
 }
