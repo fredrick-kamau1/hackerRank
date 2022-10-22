fa93f0c103816fea3e797ce6d841cd4999aa3bc2
Fred <70659811+fredrick-kamau1@users.noreply.github.com>
2022-10-22 15:45:53 -0500

    Update twoDArrays.java

--- src/hackerRank/twoDArrays.java
+++ src/hackerRank/twoDArrays.java
@@ -11,8 +11,14 @@ public class twoDArrays {
             for (int col = 0; col < nums[row].length ; col++){
                 System.out.print(nums[row][col]);
             }
-            System.out.println();
-            System.out.println();
+            System.out.println();            
         }
+        
+        for(int row = 0; row < nums.length; row++){
+            for (int col = 0; col < nums[row].length ; col++){
+                System.out.print(nums[row][col]);
+            }
+            System.out.println();
+           
     }
 }
